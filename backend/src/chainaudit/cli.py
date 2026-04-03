56d0470d599109cc5f8b89e80ac02df792c9d2a1
aizen299 <rainaaditya58@gmail.com>
2026-04-03 21:27:59 +0530

    Update chainaudit version

--- backend/src/chainaudit/cli.py
+++ backend/src/chainaudit/cli.py
@@ -787,7 +787,7 @@ Web app: https://chainaudit.vercel.app
     parser.add_argument(
         "--version",
         action="version",
-        version="%(prog)s 1.2.2",
+        version="%(prog)s 1.2.3",
     )
 
     subparsers = parser.add_subparsers(dest="command", metavar="<command>")
